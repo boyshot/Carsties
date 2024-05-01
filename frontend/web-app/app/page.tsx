4d92f4a252f8141fdd537a668d7cea07db66a337
Paulo Rocha <pauloandredarocha@gmail.com>
2024-05-01 10:24:33 -0300

    End section 6 initial front

---
+++ frontend/web-app/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Listings from "./auctions/Listings";
+
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Listings/>
+    </div>
+  );
+}
---
+++ frontend/web-app/next.config.mjs
@@ -0,0 +1,23 @@
+///** @type {import('next').NextConfig} */
+//const nextConfig = {};
+//export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /*experimental: {
+    serverActions: true
+  }, */
+  logging: {
+      fetches: {
+        fullUrl: true,
+      },
+    },
+    images: {
+      domains: [
+        'cdn.pixabay.com'
+      ]
+    }
+}
+
+export default nextConfig
