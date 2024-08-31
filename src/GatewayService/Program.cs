d1e1f88485ea34e2d47bd9ffca94e1a9ce39c5f8
Paulo Rocha <pauloandredarocha@gmail.com>
2024-08-31 15:48:15 -0300

    Section notification

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -13,7 +13,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
   op.TokenValidationParameters.NameClaimType = "username";
 });
 
+builder.Services.AddCors(op =>
+{
+  op.AddPolicy("customPolicy", b =>
+  {
+    b.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+  });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
